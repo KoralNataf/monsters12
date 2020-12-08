bff8600217daa474a43bf17434fe3d9a74eb7375
KoralNataf <koralnataf.dvlp@gmail.com>
2020-12-08 11:31:23 +0200

    Update

--- app/src/main/java/com/example/monsters/Activity_Main.java
+++ app/src/main/java/com/example/monsters/Activity_Main.java
@@ -2,121 +2,31 @@ package com.example.monsters;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.ImageButton;
-import android.widget.ImageView;
-import android.widget.TextView;
 
+public class Activity_Main extends ActivityBase {
 
-import java.util.ArrayList;
-
-public class Activity_Main extends AppCompatActivity {
-    public static final String EXTRA_KEY_WINNER="EXTRA_KEY_WINNER";
-    final int NUM_OF_MONSTERS=28;
-    private TextView main_LBL_girl_score;
-    private TextView main_LBL_boy_score;
-    private int girl_score;
-    private int boy_score;
-    private ImageView main_IMG_girl_monster;
-    private ImageView main_IMG_boy_monster;
-    private ImageButton main_BTN_blades;
-    ArrayList<Monster> monsters;
+    private MainViewController main_controller;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        findViews();
-        initViews();
-    }
-
-    private void findViews() {
-        main_LBL_girl_score = findViewById(R.id.main_LBL_girl_score);
-        main_LBL_boy_score = findViewById(R.id.main_LBL_boy_score);
-        girl_score=0;
-        boy_score=0;
-        main_IMG_girl_monster = findViewById(R.id.main_IMG_gitl_monster);
-        main_IMG_boy_monster = findViewById(R.id.main_IMG_boy_monster);
-        main_BTN_blades = findViewById(R.id.main_BTN_blades);
-        initMonsters();
-    }
-
-    /**
-     * in this function we connect between the image of the monster to the object id and his power
-     */
-    private void initMonsters() {
-        monsters = new ArrayList<>();
-        String name;
-        int id;
-        for(int i=1 ; i< NUM_OF_MONSTERS+1 ; i++){
-            name = "monster_"+ (i) +"";
-            id=getResources().getIdentifier(name,"drawable",this.getPackageName());
-            monsters.add(new Monster(id,i));
-        }
-    }
-
-    private void initViews() {
-        main_BTN_blades.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                if(!monsters.isEmpty()){
-                   playTurn();
-                    if(monsters.isEmpty())
-                        gameOver();
-                }
-            }
-        });
+        main_controller=new MainViewController(this);
 
     }
 
-    private void gameOver() {
-        Intent myIntent = new Intent(Activity_Main.this,Activity_Winner.class);
-        if(girl_score != boy_score)
-        {
-            int id;
-            if(girl_score>boy_score)
-                id=getResources().getIdentifier("elf_girl","drawable",this.getPackageName());
-            else
-                id=getResources().getIdentifier("elf_boy","drawable",this.getPackageName());
-            myIntent.putExtra(EXTRA_KEY_WINNER,id);
-        }
-        else
-            myIntent.putExtra(EXTRA_KEY_WINNER,-1);
-        startActivity(myIntent);
-        finish();
+    @Override
+    protected void onStart() {
+        super.onStart();
+        if(main_controller.isClicked())
+            main_controller.startTimer();
     }
 
-    private void playTurn() {
-        int girl_monster,boy_monster;
-        do{
-            girl_monster = (int)(Math.random()*monsters.size());
-            boy_monster = (int)(Math.random()*monsters.size());
-        }while(girl_monster == boy_monster);
-
-        //set the new monsters
-        main_IMG_girl_monster.setImageResource(monsters.get(girl_monster).getId());
-        main_IMG_boy_monster.setImageResource(monsters.get(boy_monster).getId());
-
-        //set the new score power
-        if(girl_monster > boy_monster)//girl wins in this turn
-            main_LBL_girl_score.setText((++girl_score)+ "");
-        else//boy wins in this turn
-            main_LBL_boy_score.setText((++boy_score)+ "");
-
-        if(girl_monster > boy_monster) {
-            monsters.remove(girl_monster);
-            monsters.remove(boy_monster);
-        }
-        else {
-            monsters.remove(boy_monster);
-            monsters.remove(girl_monster);
-        }
-
+    @Override
+    protected void onStop() {
+        super.onStop();
+        main_controller.stopTimer();
     }
-
-
 }
 
--- app/src/main/java/com/example/monsters/Activity_Winner.java
+++ app/src/main/java/com/example/monsters/Activity_Winner.java
@@ -9,8 +9,11 @@ import android.widget.ImageButton;
 import android.widget.ImageView;
 import android.widget.TextView;
 
-public class Activity_Winner extends AppCompatActivity {
+import com.bumptech.glide.Glide;
 
+public class Activity_Winner extends ActivityBase {
+
+    private ImageView winner_IMG_background;
     private TextView winner_LBL_title;
     private ImageView winner_IMG_player;
     private ImageButton winner_BTN_replay;
@@ -27,24 +30,30 @@ public class Activity_Winner extends AppCompatActivity {
     }
 
     private void findViews() {
+        winner_IMG_background=findViewById(R.id.winner_IMG_background);
         winner_LBL_title=findViewById(R.id.winner_LBL_title);
         winner_IMG_player=findViewById(R.id.winner_IMG_player);
         winner_BTN_replay=findViewById(R.id.winner_BTN_replay);
     }
 
     private void initViews() {
+        updateImage(this.getResources().getIdentifier(
+                "red_curtain","drawable",this.getPackageName()),winner_IMG_background);
+        updateImage(this.getResources().getIdentifier(
+                "replay","drawable",this.getPackageName()),winner_BTN_replay);
         winner_BTN_replay.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent secondIntent= new Intent(Activity_Winner.this,Activity_Main.class);
-                startActivity(secondIntent);
+                playSound(R.raw.snd_bubble);
+                Intent myIntent= new Intent(Activity_Winner.this,Activity_Start.class);
+                startActivity(myIntent);
                 finish();
             }
         });
     }
 
     private void showWinner() {
-        int id =getIntent().getIntExtra(Activity_Main.EXTRA_KEY_WINNER,0);
+        int id =getIntent().getIntExtra(MainViewController.EXTRA_KEY_WINNER,0);
         if(id == -1)
             showTie();
         else{
@@ -57,6 +66,5 @@ public class Activity_Winner extends AppCompatActivity {
         winner_IMG_player.setImageResource(id);
         winner_LBL_title.setText("TIE!");
     }
-
 
 }
